4b00412b8c8c0bbce91428d61a16517b3056adae
evanryuu <evanryuu@outlook.com>
2023-08-25 21:21:46 +0800

    feat: update to vue3.3 & example for better attrs

---
+++ packages/components/select/__tests__/test-select.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Select from '../src/select.vue'
+import SelectGeneric from '../src/select-generic.vue'
+</script>
+
+<template>
+  <Select :multiple="false" :multiple-limit="5" />
+  <SelectGeneric :multiple="false" :multiple-limit="5" />
+  <SelectGeneric multiple :multiple-limit="5" />
+</template>
+
+<style scoped></style>
